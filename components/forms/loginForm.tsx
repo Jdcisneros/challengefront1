86101819848d5b62b444bb4de5a038fad70217ac
Jorge Cisneros <126200060+Jdcisneros@users.noreply.github.com>
2025-01-24 10:50:58 -0300

    Update loginForm.tsx

--- components/forms/loginForm.tsx
+++ components/forms/loginForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setCredentials } from "../../redux/authSlice";
 import axios from "axios";
